d64bf7f84b7baaff8afb62b5c74ded9b18b93c97
cetinye <cetinye@hotmail.com>
2024-04-30 10:45:59 +0300

    building generation

--- Assets/Scripts/Game_Scripts/Chroma_City_Neon_Racing/RoadGenerator.cs
+++ Assets/Scripts/Game_Scripts/Chroma_City_Neon_Racing/RoadGenerator.cs
@@ -1,29 +1,37 @@
+using System.Collections.Generic;
 using Dreamteck.Splines;
 using UnityEngine;
 
 public class RoadGenerator : MonoBehaviour
 {
-    public SplineComputer splineComputer;
-    public Spline spline;
+    [Header("Components")]
+    public SplineComputer splineComputerRoad;
+    public SplineComputer splineComputerBuildings;
     public SplineFollower splineFollower;
     public SplineMesh splineMesh;
+
+    [Header("Variables")]
     public int pointAmount;
     public float distBetweenRoads;
 
-    // Start is called before the first frame update
+    [Header("Building Variables")]
+    [SerializeField] private float buildingOffsetToRoad;
+    [SerializeField] private Transform buildingsParent;
+    [SerializeField] private List<GameObject> buildings = new List<GameObject>();
+
     void Awake()
     {
         SpawnPoints(pointAmount);
         RandomizePointsOnX(-1f, 1f, 20);
+        CreateBuildingsSpline();
     }
 
     void SpawnPoints(int pointAmount)
     {
-        //Add a Spline Computer component to this object
-        splineComputer = gameObject.AddComponent<SplineComputer>();
-        //Create a new array of spline points
+        splineComputerRoad = gameObject.AddComponent<SplineComputer>();
+
         SplinePoint[] points = new SplinePoint[pointAmount];
-        //Set each point's properties
+
         for (int i = 0; i < points.Length; i++)
         {
             points[i] = new SplinePoint();
@@ -32,22 +40,51 @@ public class RoadGenerator : MonoBehaviour
             points[i].size = 1f;
             points[i].color = Color.white;
         }
-        //Write the points to the spline
-        splineComputer.SetPoints(points);
-        splineMesh.spline = splineComputer;
-        splineFollower.spline = splineComputer;
+
+        splineComputerRoad.SetPoints(points);
+        splineMesh.spline = splineComputerRoad;
+        splineFollower.spline = splineComputerRoad;
     }
 
     void RandomizePointsOnX(float minX, float maxX, int pointAmountToRandomize)
     {
-        SplinePoint[] points = new SplinePoint[splineComputer.pointCount];
-        points = splineComputer.GetPoints();
+        SplinePoint[] points = new SplinePoint[splineComputerRoad.pointCount];
+        points = splineComputerRoad.GetPoints();
 
         for (int i = 0; i < pointAmountToRandomize; i++)
         {
             points[i].position = new Vector3(UnityEngine.Random.Range(minX, maxX), points[i].position.y, points[i].position.z);
         }
 
-        splineComputer.SetPoints(points);
+        splineComputerRoad.SetPoints(points);
     }
+
+    void CreateBuildingsSpline()
+    {
+        SplinePoint[] points = new SplinePoint[splineComputerRoad.pointCount];
+        points = splineComputerRoad.GetPoints();
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i].position = new Vector3(points[i].position.x + buildingOffsetToRoad, -0.11f, points[i].position.z);
+        }
+
+        splineComputerBuildings.SetPoints(points);
+    }
+
+    // void PlaceBuildings()
+    // {
+    //     SplinePoint[] points = new SplinePoint[splineComputerBuildings.pointCount];
+    //     points = splineComputerBuildings.GetPoints();
+
+    //     foreach (SplinePoint point in points)
+    //     {
+    //         Instantiate(GetRandomBuilding(), point.position, Quaternion.identity, buildingsParent);
+    //     }
+    // }
+
+    // GameObject GetRandomBuilding()
+    // {
+    //     return buildings[UnityEngine.Random.Range(0, buildings.Count)];
+    // }
 }

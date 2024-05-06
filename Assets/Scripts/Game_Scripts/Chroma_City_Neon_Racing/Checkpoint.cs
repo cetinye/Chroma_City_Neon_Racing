469d6d19c8e6fd5e4b165d2aff8913e040434a8f
cetinye <cetinye@hotmail.com>
2024-05-06 10:13:20 +0300

    new checkpoint

--- Assets/Scripts/Game_Scripts/Chroma_City_Neon_Racing/Checkpoint.cs
+++ Assets/Scripts/Game_Scripts/Chroma_City_Neon_Racing/Checkpoint.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public Material colorMat;
+    [SerializeField] private float emissionIntensity;
     [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private List<Color> colors = new List<Color>();
 
@@ -17,6 +18,7 @@ public class Checkpoint : MonoBehaviour
     {
         colorMat.color = colors[UnityEngine.Random.Range(0, colors.Count)];
         meshRenderer.materials[1] = colorMat;
+        colorMat.SetColor(Shader.PropertyToID("_EmissionColor"), colorMat.color * emissionIntensity);
     }
 
     void OnTriggerEnter(Collider other)
